dac5df19176692b71fbd025339cd36c605f578c4
Jjponv <99134989+Jjponvv@users.noreply.github.com>
2024-05-03 23:57:35 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname+"/public")));
+io.on("connection", function(socket){
+    socket.on("newuser", function(username){
+        socket.broadcast.emit("update", username + " Joined to the desige chatroom!");
+    });
+    socket.on("exituser", function(username){
+        socket.broadcast.emit("update", username + " Left desige chatroom");
+    });
+    socket.on("chat", function(message){
+        socket.broadcast.emit("chat", message);
+    });
+});
+server.listen(5000);
