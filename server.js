b041f0acaf33a96d7725d98f662ac7d7b7a32683
Jjponv <99134989+Jjponvv@users.noreply.github.com>
2024-05-04 12:53:28 +0300

    Update and rename public/server.js to server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname+"/public")));
 io.on("connection", function(socket){
     socket.on("newuser", function(username){
         socket.broadcast.emit("update", username + " Joined to the desige chatroom!");
