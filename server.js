a601176c86c0e8be40cfbaf68bba5c546604a949
Jjponv <99134989+Jjponvv@users.noreply.github.com>
2024-05-04 13:03:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const server = require("http").createServer(app);
+const server = require("https").createServer(app);
 
 const io = require("socket.io")(server);
 
